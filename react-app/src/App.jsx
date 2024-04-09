8b4eb6c743a5646e7e6698b09f42c4df64edefb6
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-09 20:57:08 +1000

    Added Counter feature

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import Header from "./Header";
+
 function App() {
+  const appName = "React Counter";
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
   return (
-    <div>
-      <h1 className="text-5xl font-roboto">Roboto Counter</h1>
-      <h2 className="text-5xl font-bungee">Roboto Counter</h2>
+    <div className="font-roboto w-6/12">
+      <Header appName={appName} />
+      <h2 className="text-4xl font-bold text-center m-4">{count}</h2>
+      <div className="flex justify-center space-x-5">
+        <button onClick={increment} className="bg-red-600 p-8 rounded text-4xl">+</button>
+        <button onClick={decrement} className="bg-red-600 p-8 rounded text-4xl">-</button>
+      </div>
     </div>
   )
 }
