dd5132cc5c02ef8a1044cbd0e214abd917632c40
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-09 20:10:41 +1000

    Added initial configuration for react app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-roboto">Roboto Counter</h1>
+      <h2 className="text-5xl font-bungee">Roboto Counter</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ react-app/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      bungee: ["Bungee Spice", 'sans-serif']
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
