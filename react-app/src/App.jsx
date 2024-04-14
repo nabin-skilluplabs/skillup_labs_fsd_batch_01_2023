cd214095730e5081e45f8abaab8aedbe76d31c90
utsavcoder77 <baralutsav89@gmail.com>
2024-04-14 11:49:56 +1000

    updated app.jsx [utsav]

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,30 @@
-function App() {
-  
+import { useState } from "react";
+import Header from "./Header";
 
+function App() {
+  const appName = "React Counter";
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
   return (
-   <div>
-    <h1 className="text-5xl">Counter</h1>
+   <div className="font-serif">
+      <Header appName={appName} />
+      <h2 className="text-4xl font-bold text-center m-4">{count}</h2>
+      <div className="flex justify-center space-x-5">
+        <button onClick={increment} className="bg-red-600 p-8 rounded text-4xl">+</button>
+        <button onClick={decrement} className="bg-red-600 p-8 rounded text-4xl">-</button>
+
+      </div>
    </div>
   )
 }
 
-export default App
+export default App 
+
+
