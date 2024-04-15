75b37b003a02613b7ad7f533c47daf3cb0503528
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-15 19:45:00 +1000

    Add extra app

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,15 +3,21 @@ import Header from "./components/Header";
 export default function App() {
     const apps = [
         {
+            title: "Home",
+            path: "/"
+        },
+        {
             title: "Counter",
             path: "/counter"
         }
     ];
     return(
-        <div>
+        <div className="text-center">
             <Header appName="React Apps" />
             {
-                apps.map()
+                apps.map((app, index) => (
+                    <a className="text-amber-500 block py-2" key={index} href={app.path}>{app.title}</a> 
+                ))
             }
         </div>
     );
