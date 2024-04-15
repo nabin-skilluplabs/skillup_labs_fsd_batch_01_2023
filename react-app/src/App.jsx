77b4e2c30aab94228009452f7d3dc35a28e0a072
roka59 <154201123+roka59@users.noreply.github.com>
2024-04-15 19:26:36 +1000

    rename app.jsx to counter.jsx and create new app.jsx file

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,30 +1,18 @@
-import { useState } from "react";
-import Header from "./Header";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
-function App() {
-  const appName = "React Counter";
-  const [count, setCount] = useState(1);
-
-  const increment = () => {
-setCount(count + 1);
-  }
-
-  const decrement = () => {
-    if(count >=1){
-    setCount(count - 1);
-    }
-  }
-  return (
+export default function App() {
+    const apps = [
+        {
+            title: "Counter",
+            path: "/counter"
+        }
+    ]
+return(
     <div>
-<Header appName={appName}/>
-      <h2 className="text-4xl font-bold text-center mt-4">{count}</h2>
-      <div className="font-bold flex justify-center gap-4 text-5xl mt-5">
-        <Button symbol="+" action={increment} bg= "bg-red-600"/>
-        <Button symbol="-" action={decrement} bg= "bg-red-600"/>
-      </div>
+    <Header appName="React Apps" />
+    {
+        apps.map()
+    }
     </div>
-  )
+);
 }
-
-export default App
---
+++ react-app/src/Counter.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Button from "./components/Button";
+
+function App() {
+  const appName = "React Counter";
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+setCount(count + 1);
+  }
+
+  const decrement = () => {
+    if(count >=1){
+    setCount(count - 1);
+    }
+  }
+  return (
+    <div>
+<Header appName={appName}/>
+      <h2 className="text-4xl font-bold text-center mt-4">{count}</h2>
+      <div className="font-bold flex justify-center gap-4 text-5xl mt-5">
+        <Button symbol="+" action={increment} bg= "bg-red-600"/>
+        <Button symbol="-" action={decrement} bg= "bg-red-600"/>
+      </div>
+    </div>
+  )
+}
+
+export default App
