c6e7e78fb0de4266a54608589be025b300900a9e
utsavcoder77 <baralutsav89@gmail.com>
2024-04-15 20:08:48 +1000

    created router file[utsav]

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-import App from './App.jsx';
-import Counter from './Counter.jsx'
 
 import './index.css'
+import router from "./router"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/counter",
-    element: <Counter />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ react-app/src/router.jsx
@@ -0,0 +1,19 @@
+import {
+    RouterProvider,
+  } from "react-router-dom";
+
+import App from './App.jsx';
+import Counter from './Counter.jsx'
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/counter",
+      element: <Counter />,
+    },
+  ]);
+
+  export default router;
