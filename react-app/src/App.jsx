487e65525a090e11c72120213e1582b47944f3ef
utsavcoder77 <baralutsav89@gmail.com>
2024-04-15 18:55:46 +1000

    Added dynamic button[utsav]

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import Header from "./Header";
+import Header from "./components/Header";
+import Button from "./components/Button";
 
 function App() {
   const appName = "React Counter";
@@ -10,16 +11,20 @@ function App() {
   }
 
   const decrement = () => {
-    setCount(count - 1);
+
+    if(count >= 1) {
+      setCount(count - 1);
+    }
+    
   }
   return (
    <div className="font-serif">
       <Header appName={appName} />
       <h2 className="text-4xl font-bold text-center m-4">{count}</h2>
       <div className="flex justify-center space-x-5">
-        <button onClick={increment} className="bg-red-600 p-8 rounded text-4xl">+</button>
-        <button onClick={decrement} className="bg-red-600 p-8 rounded text-4xl">-</button>
-
+        <Button symbol="+" action={increment} bg="bg-orange-600" />
+        <Button symbol="-" action={decrement} bg="bg-red-600" />
+  
       </div>
    </div>
   )
---
+++ react-app/src/components/Button.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+function Button({symbol, action, bg}) {
+    return(
+        <button onClick={action} className={`${bg} p-8 rounded text-4xl`}>{symbol}</button>
+    )
+}
+
+export default Button;
