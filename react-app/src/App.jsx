f46f04a4240a7ec21f0df251ae1421ae93c93c3b
utsavcoder77 <baralutsav89@gmail.com>
2024-04-15 19:46:52 +1000

    updated code [utsav]

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,13 +3,23 @@ import Header from "./components/Header"
 export default function App() {
     const apps = [
         {
+            title: "Home",
+            path: "/"
+        },
+
+        {
             title: "Counter",
             path: "/counter"
         }
     ];
     return(
-        <div>
+        <div className="text-center">
             <Header appName="React App" />
+            {
+                apps.map((app, index) => (
+                    <a className="text-amber-500 block py-2" key={index} href={app.path}>{app.title}</a>
+                ))
+            }
         </div>
     )
 }
