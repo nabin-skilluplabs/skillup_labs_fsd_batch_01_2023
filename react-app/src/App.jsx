d1ad16950867da7e88382cd6f1a039e25b539d9f
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-15 19:22:39 +1000

    Added counter component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
 import Header from "./components/Header";
-import {Button} from "./components/Button";
 
-function App() {
-  const appName = "React Counter";
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  }
-
-  const decrement = () => {
-    if(count >= 1) {
-      setCount(count - 1);
-    }
-  }
-
-  return (
-    <div className="font-roboto w-6/12">
-      <Header appName={appName} />
-      <h2 className="text-4xl font-bold text-center m-4">{count}</h2>
-      <div className="flex justify-center space-x-5">
-        <Button symbol="+ADD" action={increment} bg="bg-red-600" />
-        <Button symbol="-SUB" action={decrement} bg="bg-orange-600" />
-      </div>
-    </div>
-  )
+export default function App() {
+    const apps = [
+        {
+            title: "Counter",
+            path: "/counter"
+        }
+    ];
+    return(
+        <div>
+            <Header appName="React Apps" />
+            {
+                apps.map()
+            }
+        </div>
+    );
 }
-
-export default App
