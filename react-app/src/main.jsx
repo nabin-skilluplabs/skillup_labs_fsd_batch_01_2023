cc0ece0353e066fed1eb66f0d7c8038caa3fcfc0
roka59 <154201123+roka59@users.noreply.github.com>
2024-04-15 19:51:59 +1000

    added router for counter in main.jsx file

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import Counter from "./Counter.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/counter",
+    element: <Counter />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
