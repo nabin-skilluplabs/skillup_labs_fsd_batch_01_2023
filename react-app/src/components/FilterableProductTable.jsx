bf1e376127b9f5af0efab016ff3325dff6613d1c
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-16 18:23:24 +1000

    Added product table

--- react-app/src/components/FilterableProductTable.jsx
+++ react-app/src/components/FilterableProductTable.jsx
@@ -1,9 +1,11 @@
+import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
 
 function FilterableProductTable({productData}) {
     return (
         <div className="w-96 mt-10 bg-gray-50 p-4 place-self-center">
             <SearchBar />
+            <ProductTable />
         </div>
     );
 }
---
+++ react-app/src/components/ProductTable.jsx
@@ -0,0 +1,12 @@
+function ProductTable() {
+    return (
+        <table className="w-full my-3">
+            <tr>
+                <th className="p-1">Name</th>
+                <th className="text-left p-1">Price</th>
+            </tr>
+        </table>
+    );
+}
+
+export default ProductTable;
