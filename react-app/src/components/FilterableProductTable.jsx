94d0a878f50274d2f2642b74a612cb15f81f9178
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-16 20:10:18 +1000

    Added filter feature

--- react-app/src/components/FilterableProductTable.jsx
+++ react-app/src/components/FilterableProductTable.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
 
 function FilterableProductTable({productData}) {
+    const [currentProducts, setCurrentProducts] = useState(productData);
+    
+    function filterStocked(event) {
+        const isChecked = event.target.checked;
+        if(isChecked) {
+          const filteredProducts = productData.filter(product => product.stocked);
+          setCurrentProducts(filteredProducts);
+        }
+        else {
+            setCurrentProducts(productData);
+        }
+        
+    }
+
     return (
         <div className="w-96 mt-10 bg-gray-50 p-4 place-self-center">
-            <SearchBar />
-            <ProductTable productData={productData} />
+            <SearchBar filterStocked={filterStocked} />
+            <ProductTable productData={currentProducts} />
         </div>
     );
 }
--- react-app/src/components/SearchBar.jsx
+++ react-app/src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
-function SearchBar() {
+function SearchBar({filterStocked}) {
     return(
         <div>
             <input className="block my-3 p-4 text-md w-full border-2 border-gray-600" placeholder="Search..." type="search" />
-            <label htmlFor="inStock"><input type="checkbox" id="inStock" /> Only show products in stock</label>
+            <label htmlFor="inStock"><input onClick={filterStocked} type="checkbox" id="inStock" /> Only show products in stock</label>
         </div>
     );
 }
