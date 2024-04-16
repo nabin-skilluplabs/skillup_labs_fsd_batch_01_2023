558ea84b4743c190f8b29f5d03d1a6430a009420
utsavcoder77 <baralutsav89@gmail.com>
2024-04-16 21:02:22 +1000

    added filter for search bar

--- react-app/src/FilterableProductTable.jsx
+++ react-app/src/FilterableProductTable.jsx
@@ -6,23 +6,39 @@ import SearchBar from "./SearchBar";
 function FiltarableProductTable({productData}) {
     
     const [currentProducts, setCurrentProducts] = useState(productData);
+    const [isStocked, setIsStoked] = useState(false);
+    const [query, setQuery] = useState("");
 
     function filterStocked(event) {
         const isChecked = event.target.checked;
         if(isChecked) {
             const filteredProducts = productData.filter(product => product.stocked);
             setCurrentProducts(filteredProducts);
+            setIsStoked(true);
         }
         else{
             setCurrentProducts(productData);
+            setIsStoked(false);
         }
 
+    }
+
+    function filterProducts(){
+        let filteredProducts = productData.filter(product => product.name.toLowerCase().indexOf(query.toLowerCase()) >= 0);
+        if (isStocked) {
+            filteredProducts = filteredProducts.filter(product => product.stocked)
+        }
+        setCurrentProducts(filteredProducts);
     }
 
     function searchProduct(event) {
         const query = event.target.value;
-        console.log(query);
+        setQuery(query);
+        filterProducts();
+        
     }
+
+
 
     return (
         <div className="mt-10 w-96 place-self-center p-4 bg-slate-300">
--- react-app/src/SearchBar.jsx
+++ react-app/src/SearchBar.jsx
@@ -2,7 +2,7 @@
 function SearchBar({filterStocked, searchProduct}){
     return (
         <div>
-            <input onKeyDown={searchProduct} className="block" placeholder="Search..." type="search" />
+            <input onKeyUp={searchProduct} className="block p-1 border-red-500" placeholder="Search..." type="search" />
             <label htmlFor="instock">
                 <input onClick={filterStocked} type="checkbox" id="instock" />Only Show available product
             </label>
