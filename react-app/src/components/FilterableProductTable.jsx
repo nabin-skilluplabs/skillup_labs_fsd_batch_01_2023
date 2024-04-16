f516b15ec44bbfd917aa551fc0573cc7ebaf6e23
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-16 21:01:24 +1000

    Update FilterableProductTable.jsx

--- react-app/src/components/FilterableProductTable.jsx
+++ react-app/src/components/FilterableProductTable.jsx
@@ -1,26 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
 
 function FilterableProductTable({productData}) {
     const [currentProducts, setCurrentProducts] = useState(productData);
-    
+    const [isStocked, setIsStocked] = useState(false);
+    const [query, setQuery] = useState("");
+
     function filterStocked(event) {
         const isChecked = event.target.checked;
         if(isChecked) {
-          const filteredProducts = productData.filter(product => product.stocked);
-          setCurrentProducts(filteredProducts);
+            setIsStocked(true);
         }
         else {
-            setCurrentProducts(productData);
+            setIsStocked(false)
         }
+    }
+
+    function filterProducts() {
+        let filteredProducts = productData.filter(product =>  product.name.toLowerCase().indexOf(query.toLowerCase()) >= 0);
+        if(isStocked) {
+            filteredProducts = filteredProducts.filter(product => product.stocked);
+        }
+        setCurrentProducts(filteredProducts);
     }
 
     function searchProducts(event) {
         const query = event.target.value;
-        const filteredProducts = productData.filter(product =>  product.name.toLowerCase().indexOf(query.toLowerCase()) >= 0);
-        setCurrentProducts(filteredProducts); 
+        setQuery(query);
     }
+
+    useEffect(() => {
+        filterProducts();
+    }, [query, isStocked])
 
     return (
         <div className="w-96 mt-10 bg-gray-50 p-4 place-self-center">
