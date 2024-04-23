0d87ca92f0f8a75ad8220d29dde74af20aa78fb3
utsavcoder77 <baralutsav89@gmail.com>
2024-04-23 10:59:01 +1000

    added css to the project

--- react-app/src/components/NewsItem.jsx
+++ react-app/src/components/NewsItem.jsx
@@ -1,18 +1,33 @@
-import News from "../pages/News"
-
-function NewsItem({newsItem}) {
+function NewsItem({ newsItem }) {
   return (
-    <div>
-      <img src={newsItem.banner}  />
-      <span>{newsItem.title}</span>
-      <h2>{newsItem.category}</h2>
-      <img src={newsItem. authorProfile}  />
-      <span>{newsItem.addedDate}</span>
-      <h4>{newsItem.authorName}</h4>
-
+    <div className="flex px-10">
+      <div className="flex mx-28 my-6 gap-8 flex-wrap">
+      <div>
+        <img className="w-64" src={newsItem.banner} />
+      </div>
+      <div className="flex flex-col basis-1/3">
+        <div className="flex flex-col">
+          <div>
+            <h3>{newsItem.title}</h3>
+          </div>
+          <div>
+            <h2>{newsItem.category}</h2>
+          </div>
+        </div>
+        <div className="flex justify-between gap-9">
+          <div className="flex gap-3">
+            <img className="w-5" src={newsItem.authorProfile} />
+            <h4>{newsItem.authorName}</h4>
+          </div>
+          <div>
+            <h5>{newsItem.addedDate}</h5>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+    </div>
+    
+  );
 }
 
-export default NewsItem
-
+export default NewsItem;
