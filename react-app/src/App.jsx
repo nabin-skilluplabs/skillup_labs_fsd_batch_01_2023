af98961381f72e30b3edeec79bfa79a3287887ee
utsavcoder77 <baralutsav89@gmail.com>
2024-04-23 20:04:22 +1000

    added css

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
             <Header appName="React App" />
             {
                 apps.map((app, index) => (
-                    <a className="text-amber-500 block py-2" key={index} href={app.path}>{app.title}</a>
+                    <a className="text-amber-500 block py-3 m-1 bg-slate-500" key={index} href={app.path}>{app.title}</a>
                 ))
             }
         </div>
