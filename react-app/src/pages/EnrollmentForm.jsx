f83d0ed110e5b3525978465b017fed7fa2200f54
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2024-04-29 19:10:17 +1000

    Update EnrollmentForm.jsx

--- react-app/src/pages/EnrollmentForm.jsx
+++ react-app/src/pages/EnrollmentForm.jsx
@@ -1,60 +1,63 @@
-import {useForm} from "react-hook-form";
-import {object, String} from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+// import {useForm} from "react-hook-form";
+// import {object, String} from 'yup';
+// import { yupResolver } from '@hookform/resolvers/yup';
 
 function EnrollmentForm(){
-    let formSchema = object({
-        fullName: String().required('This field is required'),
-        email: String().email('Invalid email address!'),
-        mobile: String().required("Mobile is required"),
-        address: String().required("Address is requird"),
-        resolver: yupResolver(formSchema)
+//     let formSchema = object({
+//         fullName: String().required('This field is required'),
+//         email: String().email('Invalid email address!'),
+//         mobile: String().required("Mobile is required"),
+//         address: String().required("Address is requird"),
+//         resolver: yupResolver(formSchema)
 
 
-    })
-    const{
-        register,
-        handleSubmit,
-        watch, 
-        formState: {errors},
-    } = useForm()
+//     })
+//     const{
+//         register,
+//         handleSubmit,
+//         watch, 
+//         formState: {errors},
+//     } = useForm()
 
                       
-    function onSubmit(data){
-        console.log(data);
+//     function onSubmit(data){
+//         console.log(data);
                                                                 
-    }
+//     }
                                                                                                                                        
-    return(
-        <div>                                                                                                                                                                                                                                                                                                                                                                                        
-            <h1>Studnet Enrollment Form</h1>
-            <h2>Personal Information </h2>
+//     return(
+//         <div>                                                                                                                                                                                                                                                                                                                                                                                        
+//             <h1>Studnet Enrollment Form</h1>
+//             <h2>Personal Information </h2>
 
-            <form onSubmit={handleSubmit(onSubmit)} action="">
-              <div>
-                <label htmlFor="name">Full Name</label>
-                <input className="border" type="text"  {...register("fullName")} />
+//             <form onSubmit={handleSubmit(onSubmit)} action="">
+//               <div>
+//                 <label htmlFor="name">Full Name</label>
+//                 <input className="border" type="text"  {...register("fullName")} />
                 
-              </div>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input className="border" type="email" {...register("email")} />
-              </div>
-              <div>
-                <label htmlFor="">Mobiles</label>
-                <input className="border" type="text" {...register("mobile")}/>
-              </div>
-              <div>
-                <label htmlFor="">Residential Address</label>
-                <textarea className="border" name="address" id="" cols="30" rows="10" {...register("address")}></textarea>
-              </div>
-              <div>
-                    <input className="border  " type="submit" value="Enroll Now" />
-                    <input className="border bg-gray-500 text-white" type="reset" value="Reset" />
-              </div>
+//               </div>
+//               <div>
+//                 <label htmlFor="email">Email</label>
+//                 <input className="border" type="email" {...register("email")} />
+//               </div>
+//               <div>
+//                 <label htmlFor="">Mobiles</label>
+//                 <input className="border" type="text" {...register("mobile")}/>
+//               </div>
+//               <div>
+//                 <label htmlFor="">Residential Address</label>
+//                 <textarea className="border" name="address" id="" cols="30" rows="10" {...register("address")}></textarea>
+//               </div>
+//               <div>
+//                     <input className="border  " type="submit" value="Enroll Now" />
+//                     <input className="border bg-gray-500 text-white" type="reset" value="Reset" />
+//               </div>
 
-            </form>
-        </div>
-    )
-}
-export default EnrollmentForm
+//             </form>
+//         </div>
+//     )
+return(
+    <h1>Hello</h1>
+)}
+// }
+ export default EnrollmentForm;
