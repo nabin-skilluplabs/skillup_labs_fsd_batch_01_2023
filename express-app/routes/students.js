be4173d4a971e5bb07f40fd1943d7e0247570b08
utsavcoder77 <baralutsav89@gmail.com>
2024-04-30 20:56:21 +1000

    created json file

--- express-app/routes/students.js
+++ express-app/routes/students.js
@@ -1,46 +1,14 @@
-
-import express from 'express';
+import express from "express";
+import fs from "fs";
 var router = express.Router();
 
 /* GET students listing. */
-router.get('/', function(req, res, next) {
-    const studentsRercord = [
-        {
-            name: 'Utsav Baral',
-            email: 'baralutsav89@gmail.com',
-            mobile: '0450609562',
-            intro: 'Utsav is a very nice boy...'
-        },
-
-        {
-            name: 'Ashish Roka',
-            email: 'rokaAshish@gmail.com',
-            mobile: '0450609545',
-            intro: 'Ashish is a very nice boy...'
-        },
-
-        {
-            name: 'Suraj Sharma',
-            email: 'suraj89@gmail.com',
-            mobile: '0450609561',
-            intro: 'Suraj is a very nice boy...'
-        },
-
-        {
-            name: 'Rajan Magar',
-            email: 'magarrajan433@gmail.com',
-            mobile: '0450609331',
-            intro: 'Rajan is a very nice boy...'
-        },
-
-        {
-            name: 'Anurag Kherwar',
-            email: 'Anuragkherwar333@gmail.com',
-            mobile: '0450609521',
-            intro: 'Anurag is a very nice boy...'
-        }
-    ]
-  res.json({data: studentsRercord});
+router.get("/", async function (req, res, next) {
+  const studentsRercord = await fs.readFileSync(
+    "./data/students.json",
+    "utf-8"
+  );
+  res.json({ data: JSON.parse(studentsRercord) });
 });
 
-export default router; 
+export default router;
