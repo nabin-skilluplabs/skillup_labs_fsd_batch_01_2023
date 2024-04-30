ac831cb3fa6dbf3dad8dcbfa2f7d2202549ec118
roka59 <154201123+roka59@users.noreply.github.com>
2024-04-30 20:26:44 +1000

    connected express to react

--- express_app/app.js
+++ express_app/app.js
@@ -4,13 +4,14 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import studentRouter from './routes/student.js';
 
 var app = express();
-
+app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
--- react-app/src/pages/Student.jsx
+++ react-app/src/pages/Student.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 function Student() {
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+(async() => {
+    const request = await fetch('http://localhost:3004/student');
+    const studentData = await request.json();
+    console.log(studentData);
+})()
+    },[])
   return (
     <>
       <h2>Students</h2>
+
     </>
   );
 }
