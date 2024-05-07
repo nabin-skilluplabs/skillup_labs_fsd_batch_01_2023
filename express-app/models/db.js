fbe54ac5a187471c0c24e905db25c3540c31e8ac
utsavcoder77 <baralutsav89@gmail.com>
2024-05-07 20:22:25 +1000

    additional routes

--- express-app/models/db.js
+++ express-app/models/db.js
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 let prisma;
 let _db_;
@@ -13,15 +13,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 function getClient() {
-  const { DATABASE_URL } = process.env;
-
-  const client = new PrismaClient({
-    db: {
-      url: DATABASE_URL,
-    },
-  });
+  const client = new PrismaClient();
   client.$connect();
   return client;
 }
 
-export default Prisma;
+export default prisma;
--- express-app/routes/students.js
+++ express-app/routes/students.js
@@ -1,20 +1,60 @@
-import { Prisma } from "@prisma/client";
 import express from "express";
 import fs from "fs";
+import prisma from "../models/db.js";
 
 var router = express.Router();
 
 /* GET students listing. */
 router.get("/", async function (req, res, next) {
-  const studentsRercord = await fs.readFileSync(
-    "./data/students.json",
-    "utf-8"
-  );
-  res.json({ data: JSON.parse(studentsRercord) });
+  // const studentsRercord = await fs.readFileSync(
+  //   "./data/students.json",
+  //   "utf-8"
+  // );
+  const studentsRecord = await prisma.student.findMany();
+  res.json({ data: studentsRecord });
 });
 
 router.post("/", async function (req, res, next) {
   const studentData = req.body;
+  const newStudent = await prisma.student.create({
+    data: studentData,
+  });
+
+  res.json(newStudent);
+});
+
+router.patch("/:id", async function (req, res, next) {
+  const studentData = req.body;
+  const id = req.params.id && parseInt(req.params.id);
+
+  const { id_, ...data } = studentData;
+  if (!id) {
+    return res.status(404).send(`No student found`);
+  }
+
+  const existingStudent = await prisma.student.findUnique({ where: { id } });
+  if (!existingStudent) {
+    return res.status(404).send(`No student found with id ${id}`);
+  }
+
+  const updatedStudent = await prisma.student.update({
+    where: { id },
+    data,
+  });
+  res.json(updatedStudent);
+});
+
+router.delete("/:id", async function (req, res, next) {
+  const id = req.params.id && parseInt(req.params.id);
+  if (!id) {
+    return res.status(404).send(`No student found with id ${id}`);
+  }
+
+  await prisma.student.delete({
+    where: { id },
+  });
+
+  res.status(204).send(`Student deleted with id ${id}`);
 });
 
 export default router;
